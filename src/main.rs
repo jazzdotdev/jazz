e1dc76eb6c14b8fc0d1fd1242ffeec4a477760f8
Dmitry Vdovin <voidxnull@gmail.com>
2018-08-25 22:42:54 +0200

    Move lua script into a separate file

--- src/main.rs
+++ src/main.rs
@@ -90,30 +90,7 @@ fn main() {
 
     let addr = Arbiter::start(|_| {
         LuaActorBuilder::new()
-            .on_handle_with_lua(
-                r#"
-                    host = "0.0.0.0"
-                    if ctx.msg.host then
-                        host = ctx.msg.host
-                    end
-
-                    message = "Host: " .. host .. "\n" .. ctx.msg.req_line .. "\n\nHTTP headers:\n"
-
-                    for k, v in pairs(ctx.msg.headers) do
-                        message = message .. k .. ": " .. v .. "\n"
-                    end
-
-                    message = message .. "\nRequest body:\n" .. ctx.msg.body
-
-                    print(message)
-
-                    return "<html><head><title>Hello " ..
-                        host ..
-                        "</title></head><body>Hello " ..
-                        host ..
-                        "</body></html>"
-                "#,
-            )
+            .on_handle_with_lua(include_str!("./handler.lua"))
             .build()
             .unwrap()
     });
