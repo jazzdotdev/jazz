0b44fb8ebcb148cd91dd3a0b4a03b18790ee7d7e
Michael Ravits <michael@xlucidity.com>
2018-09-07 02:15:27 +0300

    render markdown body of content files to html

--- lua/documents/helpers.lua
+++ lua/documents/helpers.lua
@@ -2,12 +2,13 @@ local function split_document(document_text, id)
     local yaml_text, body = document_text:match("(.*)\n\n(.*)")
     local yaml = yaml.load(yaml_text)
     local processed_body = body:gsub("\n", "\\n")
+    local html_body = markdown_to_html(processed_body, {safe = true})
 
     local params = {
         uuid = id,
         type = yaml.type,
         title = yaml.title,
-        body = processed_body,
+        body = html_body,
         created = yaml.created or "",
         updated = yaml.updated or "",
     }
