fbab62b3a5a927ca96053ae66c9eca00263f1d1c
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-10-01 20:59:54 -0400

    Moved all rust files out

--- actix-lua-web-app/lua/packages/initial_package/rules/post_document_rule.lua
+++
@@ -1,9 +0,0 @@
-function rule(req, events)
-    if req.method == "POST" and req.path == "/" then
-        events["reqProcess_post_document"]:trigger(req)
-    end
-end
-
-return{
-    rule = rule
-}
