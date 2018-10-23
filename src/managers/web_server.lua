c869f3905351e25416d94cdc07828633af2b7b2e
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-10-23 22:31:41 +0200

    rules-parameters working 100%

--- src/managers/web_server.lua
+++ src/managers/web_server.lua
@@ -11,7 +11,15 @@ local trace
 local status = xpcall(function()
   events["incoming_request_received"]:trigger(request)
   for k, v in pairs(rules) do
-    v.rule(request, events)
+    local rule_arguments = { }
+    for k1, v1 in pairs(v.parameters) do
+        if v1 == "events" then rule_arguments[v1] = events
+        elseif v1 == "request" then rule_arguments[v1] = request
+        -- elseif v1 == "parameter-name" then rule_arguments[v1] = parameter_value - this is how we add parameters to arugments table
+        end
+            
+    end
+    v.rule(rule_arguments)
   end
 end, function (msg)
   local trace = debug.traceback(msg, 3)
