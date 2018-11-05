c37f518c7a98a8a02decb25ea2b02de8d1ac192e
sharafzaman <sharafzaz121@gmail.com>
2018-11-06 01:36:00 +0530

    Verbosity reduced;
    Permissions error fixed

--- android-build.sh
+++ android-build.sh
@@ -11,7 +11,7 @@ rustup target add aarch64-linux-android armv7-linux-androideabi
 
 # Android NDK and toolchain 
 wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip && \
-        unzip -q android-ndk-r16b-linux-x86_64.zip && \
+        unzip -qq android-ndk-r16b-linux-x86_64.zip && \
         rm android-ndk-r16b-linux-x86_64.zip
 export NDK_HOME="android-ndk-r16b"
 
@@ -23,13 +23,13 @@ ${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
 ${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
 --arch=arm64 --install-dir=~/NDK/aarch64 --stl=libc++ --platform=android-26
 
-echo '[target.arm-linux-androideabi] 
+sudo sh -c "echo '[target.arm-linux-androideabi] 
 ar = "arm-linux-androideabi-ar" 
 linker = "arm-linux-androideabi-clang" 
  
 [target.aarch64-linux-android] 
 ar = "aarch64-linux-android-ar" 
-linker = "aarch64-linux-android-clang"' > ~/.cargo/config
+linker = "aarch64-linux-android-clang"' >> ~/.cargo/config"
 
 export PATH="$PATH:~/NDK/arm/bin" 
 export PATH="$PATH:~/NDK/aarch64/bin"
