0787a13ce26331e696d950fb8af16bde6e64f7cc
sharafzaman <sharafzaz121@gmail.com>
2018-11-05 23:31:04 +0530

    Installing directory changed

--- android-build.sh
+++ android-build.sh
@@ -6,23 +6,22 @@ sudo apt-get update && \
 
 # # Rust & Cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-export PATH="/root/.cargo/bin:$PATH"
+export PATH="~/.cargo/bin:$PATH"
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
 
-# Android NDK and toolchain
-cd /usr/local && \
-        wget  https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip && \
+# Android NDK and toolchain 
+wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip && \
         unzip -q android-ndk-r16b-linux-x86_64.zip && \
         rm android-ndk-r16b-linux-x86_64.zip
-export NDK_HOME="/usr/local/android-ndk-r16b"
+export NDK_HOME="android-ndk-r16b"
 
 # build toolchain for arm 
 ${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
---arch=arm --install-dir=/opt/NDK/arm --stl=libc++ --platform=android-26
+--arch=arm --install-dir=~/NDK/arm --stl=libc++ --platform=android-26
 
 # build toolchain for aarch64 
 ${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
---arch=arm64 --install-dir=/opt/NDK/aarch64 --stl=libc++ --platform=android-26
+--arch=arm64 --install-dir=~/NDK/aarch64 --stl=libc++ --platform=android-26
 
 echo '[target.arm-linux-androideabi] 
 ar = "arm-linux-androideabi-ar" 
@@ -34,6 +33,6 @@ linker = "aarch64-linux-android-clang"' > ~/.cargo/config
 ~
 ~
 
-export PATH="$PATH:/opt/NDK/arm/bin" 
-export PATH="$PATH:/opt/NDK/aarch64/bin"
+export PATH="$PATH:~/NDK/arm/bin" 
+export PATH="$PATH:~/NDK/aarch64/bin"
 cd $RUNNING_HOME 
