e616b0a6a9da393eecc357e2ec8e041ed203cf64
sharafzaman <sharafzaz121@gmail.com>
2018-11-06 02:04:50 +0530

    Compilation error fixed
    Less verbose

--- android-build.sh
+++ android-build.sh
@@ -10,7 +10,7 @@ export PATH="~/.cargo/bin:$PATH"
 rustup target add aarch64-linux-android armv7-linux-androideabi
 
 # Android NDK and toolchain 
-wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip && \
+wget -q https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip && \
         unzip -qq android-ndk-r16b-linux-x86_64.zip && \
         rm android-ndk-r16b-linux-x86_64.zip
 export NDK_HOME="android-ndk-r16b"
@@ -24,12 +24,12 @@ ${NDK_HOME}/build/tools/make-standalone-toolchain.sh \
 --arch=arm64 --install-dir=~/NDK/aarch64 --stl=libc++ --platform=android-26
 
 sudo sh -c "echo '[target.arm-linux-androideabi] 
-ar = "arm-linux-androideabi-ar" 
-linker = "arm-linux-androideabi-clang" 
+ar = \"arm-linux-androideabi-ar\" 
+linker = \"arm-linux-androideabi-clang\" 
  
 [target.aarch64-linux-android] 
-ar = "aarch64-linux-android-ar" 
-linker = "aarch64-linux-android-clang"' >> ~/.cargo/config"
+ar = \"aarch64-linux-android-ar\" 
+linker = \"aarch64-linux-android-clang\"' >> ~/.cargo/config"
 
 export PATH="$PATH:~/NDK/arm/bin" 
 export PATH="$PATH:~/NDK/aarch64/bin"
