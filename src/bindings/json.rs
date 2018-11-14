7f6b28693a08c047dc29fd611c751051ad72acc4
Darius Clark <darius.clark93@gmail.com>
2018-11-14 09:02:46 -0500

    Cleaned up json and yaml

--- src/bindings/json.rs
+++ src/bindings/json.rs
@@ -4,7 +4,8 @@ use rlua_serde;
 
 pub fn init(lua: &Lua) -> Result<(), LuaError> {
     // Decode string to a table
-    let to_table = lua.create_function(|lua, text: String| {
+    let module = lua.create_table()?;
+    module.set("to_table", lua.create_function(|lua, text: String| {
         let doc: serde_json::Value = serde_json::from_str(&text)
             .map_err(|err| {
                 LuaError::external(err)
@@ -12,10 +13,10 @@ pub fn init(lua: &Lua) -> Result<(), LuaError> {
         let lua_value = rlua_serde::to_value(lua, &doc)?;
 
         Ok(lua_value)
-    })?;
+    })?)?;
 
     // Encode table to a string
-    let from_table = lua.create_function(|_, value: LuaValue| {
+    module.set("from_table", lua.create_function(|_, value: LuaValue| {
         let lua_value: serde_json::Value = rlua_serde::from_value(value)?;
         let string = serde_json::to_string(&lua_value)
             .map_err(|err| {
@@ -23,11 +24,7 @@ pub fn init(lua: &Lua) -> Result<(), LuaError> {
             })?;
 
         Ok(string)
-    })?;
-
-    let module = lua.create_table()?;
-    module.set("to_table", to_table)?;
-    module.set("from_table", from_table)?;
+    })?)?;
 
     lua.globals().set("json", module)?;
 
