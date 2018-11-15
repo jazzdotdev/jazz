7eafe4fe334e97bc9546c3fe3d318fb7d3af0435
Do Duy <juchiast@gmail.com>
2018-11-15 13:10:23 +0700

    write test

--- src/bindings/tantivy.rs
+++ src/bindings/tantivy.rs
@@ -45,7 +45,7 @@ fn to_text_option(vec: Vec<rlua::Value>) -> rlua::Result<TextOptions> {
 struct Field(tantivy::schema::Field);
 impl UserData for Field {}
 
-struct SchemaBuilder(tantivy::schema::SchemaBuilder);
+struct SchemaBuilder(Option<tantivy::schema::SchemaBuilder>);
 
 impl UserData for IntOptions {}
 
@@ -55,7 +55,8 @@ impl UserData for SchemaBuilder {
             "add_u64_field",
             |_, this, (name, options): (String, Vec<rlua::Value>)| {
                 let option = to_int_option(options)?;
-                let field = this.0.add_u64_field(&name, option.0);
+                let this = this.0.as_mut().expect("Value already moved");
+                let field = this.add_u64_field(&name, option.0);
                 Ok(Field(field))
             },
         );
@@ -64,7 +65,8 @@ impl UserData for SchemaBuilder {
             "add_i64_field",
             |_, this, (name, options): (String, Vec<rlua::Value>)| {
                 let option = to_int_option(options)?;
-                let field = this.0.add_i64_field(&name, option.0);
+                let this = this.0.as_mut().expect("Value already moved");
+                let field = this.add_i64_field(&name, option.0);
                 Ok(Field(field))
             },
         );
@@ -73,21 +75,79 @@ impl UserData for SchemaBuilder {
             "add_text_field",
             |_, this, (name, options): (String, Vec<rlua::Value>)| {
                 let option = to_text_option(options)?;
-                let field = this.0.add_text_field(&name, option.0);
+                let this = this.0.as_mut().expect("Value already moved");
+                let field = this.add_text_field(&name, option.0);
                 Ok(Field(field))
             },
         );
 
         methods.add_method_mut("add_facet_field", |_, this, name: String| {
-            Ok(Field(this.0.add_facet_field(&name)))
+            let this = this.0.as_mut().expect("Value already moved");
+            Ok(Field(this.add_facet_field(&name)))
         });
 
         methods.add_method_mut("add_bytes_field", |_, this, name: String| {
-            Ok(Field(this.0.add_facet_field(&name)))
+            let this = this.0.as_mut().expect("Value already moved");
+            Ok(Field(this.add_facet_field(&name)))
+        });
+
+        methods.add_method_mut("build", |_, this, _: ()| {
+            let this = this.0.take().expect("Value already moved");
+            Ok(Schema(this.build()))
+        })
+    }
+}
+
+struct Schema(tantivy::schema::Schema);
+
+impl UserData for Schema {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("get_field", |_, this, name: String| {
+            Ok(this.0.get_field(&name).map(|x| Field(x)))
         });
     }
 }
 
+struct Index(tantivy::Index);
+
+impl UserData for Index {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("writer", |_, this, size: usize| {
+            Ok(IndexWriter(this.0.writer(size).expect("Error getting writer")))
+        });
+    }
+}
+
+struct IndexWriter(tantivy::IndexWriter);
+
+impl UserData for IndexWriter {}
+
 pub fn init(lua: &Lua) -> Result<(), LuaError> {
+    let tan = lua.create_table()?;
+    tan.set("new_schema_builder", lua.create_function(|_, _: ()| {
+        Ok(SchemaBuilder(Default::default()))
+    })?)?;
+
+    let globals = lua.globals();
+    globals.set("tan", tan)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use rlua::{Lua, Value};
+    static SCRIPT: &str = r##"
+    local builder = tan.new_schema_builder()
+    "##;
+    #[test]
+    fn test() {
+        let lua = Lua::new();
+        super::init(&lua).unwrap();
+
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.path().as_os_str().to_str().unwrap();
+        let globals = lua.globals();
+        globals.set("index_path", path).unwrap();
+        lua.exec::<_, Value>(SCRIPT, None).unwrap();
+    }
 }
