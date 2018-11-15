6e3d0b9422bcc178acc95d777df4213700dfb85b
Do Duy <juchiast@gmail.com>
2018-11-15 16:51:29 +0700

    completed

--- src/bindings/tantivy.rs
+++ src/bindings/tantivy.rs
@@ -98,9 +98,13 @@ impl UserData for Schema {
         methods.add_method("get_field", |_, this, name: String| {
             Ok(this.0.get_field(&name).map(|x| Field(x)))
         });
+        methods.add_method("to_json", |_, this, doc: Document| {
+            Ok(this.0.to_json(&doc.0))
+        });
     }
 }
 
+#[derive(Clone)]
 struct Index(tantivy::Index);
 
 impl UserData for Index {
@@ -110,8 +114,57 @@ impl UserData for Index {
                 this.0.writer(size).expect("Error getting writer"),
             ))
         });
+        methods.add_method("load_searchers", |_, this, _: ()| {
+            this.0.load_searchers().expect("load_searchers failed");
+            Ok(())
+        });
+        methods.add_method(
+            "search",
+            |_, this, (p, q, a): (QueryParser, String, rlua::AnyUserData)| {
+                if a.is::<TopCollector>() {
+                    Ok(this.search(p, q, &mut *a.borrow_mut::<TopCollector>().unwrap()))
+                } else {
+                    Err(rlua::Error::UserDataTypeMismatch)
+                }
+            },
+        );
     }
 }
+
+impl Index {
+    fn search<T: GetCollector>(&self, p: QueryParser, q: String, c: &mut T) -> Vec<Document> {
+        let searcher = self.0.searcher();
+        let query = p.0.parse_query(&q).unwrap();
+        searcher.search(&*query, c.get_collector()).unwrap();
+        c.get_doc_addresses()
+            .into_iter()
+            .map(|x| Document(searcher.doc(x).unwrap()))
+            .collect()
+    }
+}
+
+trait GetCollector {
+    type Output: tantivy::collector::Collector;
+    fn get_collector(&mut self) -> &mut Self::Output;
+    fn get_doc_addresses(&self) -> Vec<tantivy::DocAddress>;
+}
+
+#[derive(Clone)]
+struct TopCollector(std::sync::Arc<tantivy::collector::TopCollector>);
+impl UserData for TopCollector {}
+impl GetCollector for TopCollector {
+    type Output = tantivy::collector::TopCollector;
+    fn get_collector(&mut self) -> &mut Self::Output {
+        std::sync::Arc::get_mut(&mut self.0).unwrap()
+    }
+    fn get_doc_addresses(&self) -> Vec<tantivy::DocAddress> {
+        self.0.docs()
+    }
+}
+
+#[derive(Clone)]
+struct QueryParser(std::sync::Arc<tantivy::query::QueryParser>);
+impl UserData for QueryParser {}
 
 struct IndexWriter(tantivy::IndexWriter);
 
@@ -119,6 +172,9 @@ impl UserData for IndexWriter {
     fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
         methods.add_method_mut("add_document", |_, this, doc: Document| {
             Ok(this.0.add_document(doc.0))
+        });
+        methods.add_method_mut("commit", |_, this, _: ()| {
+            Ok(this.0.commit().expect("IndexWriter commit failed"))
         });
     }
 }
@@ -131,7 +187,7 @@ impl UserData for Document {
         methods.add_method_mut("add_text", |_, this, (f, t): (Field, String)| {
             this.0.add_text(f.0, &t);
             Ok(())
-        })
+        });
     }
 }
 
@@ -150,16 +206,43 @@ pub fn init(lua: &Lua) -> Result<(), LuaError> {
     tan.set("FAST", IntOptions(tantivy::schema::FAST))?;
     tan.set("FACET_SEP_BYTE", tantivy::schema::FACET_SEP_BYTE)?;
 
-    tan.set("index_in_ram", lua.create_function(|_, schema: Schema| {
-        Ok(Index(tantivy::Index::create_in_ram(schema.0)))
-    })?)?;
-    tan.set("index_in_dir", lua.create_function(|_, (path, schema): (String, Schema)| {
-        Ok(Index(tantivy::Index::create_in_dir(&path ,schema.0).expect("create_in_dir failed")))
-    })?)?;
+    tan.set(
+        "index_in_ram",
+        lua.create_function(|_, schema: Schema| {
+            Ok(Index(tantivy::Index::create_in_ram(schema.0)))
+        })?,
+    )?;
+    tan.set(
+        "index_in_dir",
+        lua.create_function(|_, (path, schema): (String, Schema)| {
+            Ok(Index(
+                tantivy::Index::create_in_dir(&path, schema.0).expect("create_in_dir failed"),
+            ))
+        })?,
+    )?;
 
-    tan.set("new_document", lua.create_function(|_, _: ()| {
-        Ok(Document(Default::default()))
-    })?)?;
+    tan.set(
+        "new_document",
+        lua.create_function(|_, _: ()| Ok(Document(Default::default())))?,
+    )?;
+
+    tan.set(
+        "top_collector_with_limit",
+        lua.create_function(|_, l: usize| {
+            Ok(TopCollector(std::sync::Arc::new(
+                tantivy::collector::TopCollector::with_limit(l),
+            )))
+        })?,
+    )?;
+    tan.set(
+        "query_parser_for_index",
+        lua.create_function(|_, (i, f): (Index, Vec<Field>)| {
+            let f = f.into_iter().map(|x| x.0).collect();
+            Ok(QueryParser(std::sync::Arc::new(
+                tantivy::query::QueryParser::for_index(&i.0, f),
+            )))
+        })?,
+    )?;
 
     let globals = lua.globals();
     globals.set("tan", tan)?;
@@ -190,6 +273,15 @@ mod tests {
     doc:add_text(body, "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
     index_writer:add_document(doc)
 
+    index_writer:commit()
+    index:load_searchers()
+
+    local parser = tan.query_parser_for_index(index, {title, body})
+    local coll = tan.top_collector_with_limit(10)
+    local result = index:search(parser, "laboris", coll)
+    for i = 1, #result do
+        print(schema:to_json(result[i]))
+    end
     "##;
     #[test]
     fn test() {
