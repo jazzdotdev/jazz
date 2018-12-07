412a7deacf06ab62eac5c9223b7c022ffae1015f
Mykyta Holubakha <hilobakho@gmail.com>
2018-12-07 20:42:49 +0200

    Added a blake2 hash function to crypto module

--- src/bindings/crypto/hash.rs
+++ src/bindings/crypto/hash.rs
@@ -1,9 +1,18 @@
 use rlua::{Error as LuaError, Lua};
+use blake2::*;
 use rust_sodium::crypto::hash;
 use base64;
 
 /// Returns base64 encoded SHA-512 of `msg`
 pub fn hash(_lua: &Lua, msg: String) -> Result<String, LuaError> {
     let digest = hash::hash(msg.as_bytes());
+    Ok(base64::encode(&digest))
+}
+
+/// Returns base64 encoded BLAKE2B of `msg`
+pub fn blake2_hash(_lua: &Lua, msg: String) -> Result<String, LuaError> {
+    let mut hasher = Blake2b::new();
+    hasher.input(msg);
+    let digest = hasher.result();
     Ok(base64::encode(&digest))
 }
