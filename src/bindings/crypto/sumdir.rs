d954aa625f1e9b59c2ec50a956a882bbea1eb590
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-09 01:01:56 +0530

    Updated for latest version of `sumdir` crate

--- src/bindings/crypto/sumdir.rs
+++ src/bindings/crypto/sumdir.rs
@@ -2,5 +2,6 @@ use rlua::{Error as LuaError, Lua};
 use sumdir;
 
 pub fn gen_checksum(_lua: &Lua, path: String) -> Result<String, LuaError> {
-    Ok(sumdir::dir_hash(&path[..]))
+    sumdir::dir_hash(&path[..])
+        .map_err(LuaError::external)
 }
