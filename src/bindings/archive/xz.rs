d8e1ce03d7677f52f2da1c1a0a32233aae80e28e
Darius Clark <darius.clark93@gmail.com>
2019-02-18 13:06:15 -0500

    Implements a compress binding for xz
    TODO: Expose the XzEncoder/XzDecoder to make better use of the functionality and implement similar functionality for other bindings under archive
    
    Related to #257

--- src/bindings/archive/xz.rs
+++ src/bindings/archive/xz.rs
@@ -1,13 +1,22 @@
 use rlua::prelude::*;
 use std::fs;
-use std::io::Read;
+use std::io::{self, Read};
 use xz2::read::*;
 use super::ByteBuf;
 
 pub fn init(lua: &Lua) -> crate::Result<()> {
     lua.context(|lua| {
         let module = lua.create_table()?;
-        module.set("decompress", lua.create_function(|_, file: (String)| {
+
+        module.set("compress", lua.create_function(|_, (input, output, level): (String, String, Option<u32>)| {
+            let level = level.unwrap_or(6);
+            let file = fs::File::open(&input).map_err(LuaError::external)?;
+            let mut output = fs::File::create(&output).map_err(LuaError::external)?;
+            let mut data = XzEncoder::new(file, level);
+            io::copy(&mut data, &mut output).map_err(LuaError::external)
+        })?)?;
+
+        module.set("decompress", lua.create_function(|_, file: String| {
             let file = fs::File::open(&file).map_err(LuaError::external)?;
             let mut data = XzDecoder::new(file);
             let mut buf = vec![];
