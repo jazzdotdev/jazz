7e626018d65f86219e2afa1ac244007387dca3f8
Darius Clark <darius.clark93@gmail.com>
2019-02-18 13:29:52 -0500

    Fixed combinediff and interdiff to work with rlua 0.16

--- src/bindings/text/combinediff.rs
+++ src/bindings/text/combinediff.rs
@@ -1,4 +1,4 @@
-use rlua::Lua;
+use rlua::prelude::*;
 
 use std::{io, fs};
 use patch_rs::{Patch, PatchProcessor};
@@ -6,37 +6,40 @@ use patch_rs::{Patch, PatchProcessor};
 use super::NULL_SOURCE;
 
 pub fn init(lua: &Lua) -> crate::Result<()> {
-    let module = lua.create_table()?;
 
-    module.set(
-        "combinediff",
-        lua.create_function(|_, (patch_1, patch_2): (String, String)| {
-            if patch_1 == NULL_SOURCE && patch_2 == NULL_SOURCE {
-                return Err(rlua::Error::external(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "Both patches cannot be null"
-                )));
-            }
+    lua.context(|lua| {
+        let module = lua.create_table()?;
+        module.set(
+            "combinediff",
+            lua.create_function(|_, (patch_1, patch_2): (String, String)| {
+                if patch_1 == NULL_SOURCE && patch_2 == NULL_SOURCE {
+                    return Err(LuaError::external(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        "Both patches cannot be null"
+                    )));
+                }
 
-            let patch_1 = if patch_1 != NULL_SOURCE {
-                let patch_1 = fs::read_to_string(patch_1).map_err(rlua::Error::external)?;
-                PatchProcessor::convert(&patch_1).map_err(rlua::Error::external)?
-            } else {
-                Patch::default()
-            };
+                let patch_1 = if patch_1 != NULL_SOURCE {
+                    let patch_1 = fs::read_to_string(patch_1).map_err(LuaError::external)?;
+                    PatchProcessor::convert(&patch_1).map_err(crate::error::Error::PatchError).map_err(LuaError::external)?
+                } else {
+                    Patch::default()
+                };
 
-            let patch_2 = if patch_2 != NULL_SOURCE {
-                let patch_2 = fs::read_to_string(patch_2).map_err(rlua::Error::external)?;
-                PatchProcessor::convert(&patch_2).map_err(rlua::Error::external)?
-            } else {
-                Patch::default()
-            };
+                let patch_2 = if patch_2 != NULL_SOURCE {
+                    let patch_2 = fs::read_to_string(patch_2).map_err(LuaError::external)?;
+                    PatchProcessor::convert(&patch_2).map_err(crate::error::Error::PatchError).map_err(LuaError::external)?
+                } else {
+                    Patch::default()
+                };
 
-            Ok(combinediff_rs::combinediff(patch_1, patch_2, 3).to_string())
-        })?,
-    )?;
+                Ok(combinediff_rs::combinediff(patch_1, patch_2, 3).to_string())
+            })?,
+        )?;
 
-    lua.globals().set("combinediff", module)?;
+        lua.globals().set("combinediff", module)?;
 
-    Ok(())
+
+        Ok(())
+    })
 }
