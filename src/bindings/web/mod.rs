9582608257f4b1bc74d82ad43e29a5bee171f3ef
Darius Clark <darius.clark93@gmail.com>
2019-02-18 17:03:12 -0500

    Removed sass
    Due to the crate not building on musl and not being supported for android, it would be better to remove it for the time being until a better alternative is found

--- src/bindings/web/mod.rs
+++ src/bindings/web/mod.rs
@@ -1,12 +1,11 @@
 pub mod client;
 pub mod server;
-pub mod sass;
 
 use rlua::prelude::*;
 use crate::Result;
 
 pub fn init(lua: &Lua) -> Result<()> {
     client::init(lua)?;
-    sass::init(lua)?;
+
     Ok(())
 }
